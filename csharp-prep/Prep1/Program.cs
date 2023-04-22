e2fe4da691fcf5cbc1c4091d98f544608ca2f7cb
rixhon <rixhon@gmail.com>
2023-04-21 21:32:46 -0300

    assignments Prep 1 and Prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user what is your first name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask the user what is their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Show the full name.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
